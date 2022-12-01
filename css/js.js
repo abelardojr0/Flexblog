80959d0f1fbf92315c35de4fb2c3338cdda43240
Abelardo Júnior <abelardo.junior.181@gmail.com>
2022-11-30 21:16:08 -0300

    Projeto de curso, para praticar flexbox

---
+++ css/js.js
@@ -0,0 +1,10 @@
+// Retorne o url da página atual utilizando o objeto window
+const href = location.href;
+console.log(href);
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+
+// Retorne a linguagem do navegador
+const linguagem = navigator.language;
+console.log(linguagem);
+// Retorne a largura da janela
